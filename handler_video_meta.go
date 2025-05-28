2c317d0f04b3e6420ba3d537c17b3bcb5194a7ae
BadgerCannon <52602860+BadgerCannon@users.noreply.github.com>
2025-05-28 11:12:50 +0100

    Cloudfront

--- handler_video_meta.go
+++ handler_video_meta.go
@@ -95,12 +95,6 @@ func (cfg *apiConfig) handlerVideoGet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	video, err = cfg.dbVideoToSignedVideo(video)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Failed to create presigned URL", err)
-		return
-	}
-
 	respondWithJSON(w, http.StatusOK, video)
 }
 
@@ -121,14 +115,6 @@ func (cfg *apiConfig) handlerVideosRetrieve(w http.ResponseWriter, r *http.Reque
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve videos", err)
 		return
-	}
-
-	for i, v := range videos {
-		videos[i], err = cfg.dbVideoToSignedVideo(v)
-		if err != nil {
-			respondWithError(w, http.StatusInternalServerError, "Failed to create presigned URL", err)
-			return
-		}
 	}
 
 	respondWithJSON(w, http.StatusOK, videos)
